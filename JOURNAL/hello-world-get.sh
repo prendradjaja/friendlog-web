d22992acf093968e4249b9e8384d4c7439c906ad
Pandu Rendradjaja <prendradjaja@gmail.com>
2019-02-16 13:29:39 -0800

    Remove everything so I can use gh-pages branch

--- JOURNAL/hello-world-get.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-pwd
-. ./creds.sh
-SPREADSHEET_ID=1_NXaTShS4WSieqo7CrJQJWjhuJZIkYzE9ZS3KSfj_-c
-curl https://sheets.googleapis.com/v4/spreadsheets/$SPREADSHEET_ID/values/A1:D8?key=$API_KEY
--- README.md
+++
@@ -1,7 +0,0 @@
-## How to do a version bump
-
-- Update `JOURNAL.md` (No changelog for now)
-- Update `package.json`
-- `git tag vA.B.C`
-- `git push`
-- `git push --tags`
