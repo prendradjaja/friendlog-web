50651eaa54b059f652cdcb3745a08f5d1fa10797
Pandu Rendradjaja <prendradjaja@gmail.com>
2019-03-16 22:52:36 -0700

    A couple adaptations

--- friendlog-web/calendar-view/calendar-view.component.ts
+++ friendlog-web/calendar-view/calendar-view.component.ts
@@ -82,10 +82,12 @@ export class CalendarViewComponent implements OnInit, OnChanges {
         if (todaysEvents) {
           const colors = new Set();
           todaysEvents.forEach(row => {
-            colors.add(
-              this.colorService.getColor(row.who)
-              // 'blue'
-            )
+            if (row.who) {
+              colors.add(
+                this.colorService.getColor(row.who)
+                // 'blue'
+              )
+            }
           });
           if (colors.size === 1) {
             day.color = oneItemSetToItem(colors);
@@ -94,7 +96,9 @@ export class CalendarViewComponent implements OnInit, OnChanges {
             // todo the more frequent one should be on top
             [day.rightColor, day.topColor] = twoItemSetToArray(colors);
           } else {
-            day.color = 'black';
+            day.multiColor = true;
+            day.topColor = getAnyItem(colors);
+            day.rightColor = 'white';
           }
           day.numEvents = todaysEvents.length;
 
@@ -154,6 +158,16 @@ function twoItemSetToArray<T>(s: Set<T>): T[] {
     let items = [] as T[];
     s.forEach(x => items.push(x));
     return items;
+  }
+}
+
+function getAnyItem<T>(s: Set<T>): T[] {
+  if (s.size < 1) {
+    window.alert('this set is empty')
+  } else {
+    let items = []
+    s.forEach(x => items.push(x));
+    return items[0];
   }
 }
 
