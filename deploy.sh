8e05c5c483f788c488a529d1af62fbd783faaadc
Pandu Rendradjaja <prendradjaja@gmail.com>
2019-03-29 15:13:14 -0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 set -ex
 
-echo fix me
+DIR_FRIENDLOG=~/personal/friendlog-web
+DIR_FRIENDLOG_DIST=~/personal/friendlog-web/dist/friendlog-web
+DIR_FRIENDLOG_GHPAGES=~/personal/friendlog-web.gh-pages
 
-# DIR_FRIENDLOG=~/personal/friendlog-web
-# DIR_FRIENDLOG_ANGULAR=~/personal/friendlog-web/friendlog-web
-# DIR_FRIENDLOG_DIST=~/personal/friendlog-web/friendlog-web/dist/friendlog-web
-# DIR_FRIENDLOG_GHPAGES=~/personal/friendlog-web.gh-pages
-#
-# cd $DIR_FRIENDLOG_ANGULAR
-# npx ng build --prod --base-href "https://prendradjaja.github.io/friendlog-web/"
-#
-# rm -f $DIR_FRIENDLOG_GHPAGES/*
-# cp $DIR_FRIENDLOG_DIST/* $DIR_FRIENDLOG_GHPAGES
-#
-# set +x
-#
-# echo
-# echo Done. Now:
-# echo - cd to $DIR_FRIENDLOG_GHPAGES
-# echo - commit and push
+cd $DIR_FRIENDLOG
+npx ng build --prod --base-href "https://prendradjaja.github.io/friendlog-web/"
+
+rm -f $DIR_FRIENDLOG_GHPAGES/*
+cp $DIR_FRIENDLOG_DIST/* $DIR_FRIENDLOG_GHPAGES
+
+set +x
+
+echo
+echo Done. Now:
+echo - cd to $DIR_FRIENDLOG_GHPAGES
+echo - commit and push
